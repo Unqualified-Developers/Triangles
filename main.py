b15252d3e2b5b28eedf3da280c1c0ab11f3ee449
Aviation Engineer <109074663+YBCA7@users.noreply.github.com>
2024-03-03 20:20:49 +0800

    Update main.py.

--- main.py
+++ main.py
@@ -5,91 +5,90 @@ from ttkbootstrap import Radiobutton, Label, Entry, Button
 from ttkbootstrap import Window, IntVar
 
 # Do some initializations.
-r = Window()
-r.title("Triangles")
-r.resizable(False, False)
-al = Label(r, text="Edge a:")
-bl = Label(r, text="Edge b:")
-cl = Label(r, text="Edge c:")
-Al = Label(r, text="∠A(°):")
-Bl = Label(r, text="∠B(°):")
-Cl = Label(r, text="∠C(°):")
-ae = Entry(r)
-be = Entry(r)
-ce = Entry(r)
-Ae = Entry(r)
-Be = Entry(r)
-Ce = Entry(r)
-iv = IntVar()
+root = Window()
+root.title("Triangles")
+root.resizable(False, False)
+edge_a_label = Label(root, text="Edge a:")
+edge_b_label = Label(root, text="Edge b:")
+edge_c_label = Label(root, text="Edge c:")
+angle_a_label = Label(root, text="∠A(°):")
+angle_b_label = Label(root, text="∠B(°):")
+angle_c_label = Label(root, text="∠C(°):")
+edge_a_entry = Entry(root)
+edge_b_entry = Entry(root)
+edge_c_entry = Entry(root)
+angle_a_entry = Entry(root)
+angle_b_entry = Entry(root)
+angle_c_entry = Entry(root)
+int_var = IntVar()
 
 
 def sas():
     # The change of the user interface after pressing the radiobutton "SAS"
 
     # Labels
-    al.grid(row=0, column=2)
-    bl.grid(row=1, column=2)
-    Cl.grid(row=2, column=2)
-    Al.grid_forget()
-    Bl.grid_forget()
-    cl.grid_forget()
+    edge_a_label.grid(row=0, column=2)
+    edge_b_label.grid(row=1, column=2)
+    angle_c_label.grid(row=2, column=2)
+    angle_a_label.grid_forget()
+    angle_b_label.grid_forget()
+    edge_c_label.grid_forget()
 
     # Entries
-    ae.grid(row=0, column=3)
-    be.grid(row=1, column=3)
-    Ce.grid(row=2, column=3)
-    Ae.grid_forget()
-    Be.grid_forget()
-    ce.grid_forget()
+    edge_a_entry.grid(row=0, column=3)
+    edge_b_entry.grid(row=1, column=3)
+    angle_c_entry.grid(row=2, column=3)
+    angle_a_entry.grid_forget()
+    angle_b_entry.grid_forget()
+    edge_c_entry.grid_forget()
 
 
 def sss():
     # The change of the user interface after pressing the radiobutton "SSS"
 
     # Labels
-    al.grid(row=0, column=2)
-    bl.grid(row=1, column=2)
-    cl.grid(row=2, column=2)
-    Al.grid_forget()
-    Bl.grid_forget()
-    Cl.grid_forget()
+    edge_a_label.grid(row=0, column=2)
+    edge_b_label.grid(row=1, column=2)
+    edge_c_label.grid(row=2, column=2)
+    angle_a_label.grid_forget()
+    angle_b_label.grid_forget()
+    angle_c_label.grid_forget()
 
     # Entries
-    ae.grid(column=3)
-    be.grid(row=1, column=3)
-    ce.grid(row=2, column=3)
-    Ae.grid_forget()
-    Be.grid_forget()
-    Ce.grid_forget()
-
+    edge_a_entry.grid(column=3)
+    edge_b_entry.grid(row=1, column=3)
+    edge_c_entry.grid(row=2, column=3)
+    angle_a_entry.grid_forget()
+    angle_b_entry.grid_forget()
+    angle_c_entry.grid_forget()
 
 sas()  # The default option is "SAS".
-Radiobutton(r, text="SAS", variable=iv, value=0, command=sas).grid(row=0, column=0)
-Radiobutton(r, text="SSS", variable=iv, value=1, command=sss).grid(row=1, column=0)
+Radiobutton(root, text="SAS", variable=int_var, value=0, command=sas).grid(row=0, column=0)
+Radiobutton(root, text="SSS", variable=int_var, value=1, command=sss).grid(row=1, column=0)
 
 
-def p():
+def process():
     try:
-        if iv.get() == 0:  # When choosing option "SAS"...
-            C = float(Ce.get())
-            if C < 180:  # The angle of a triangle is never bigger than 180°.
-                a = float(ae.get())
-                b = float(be.get())
-                radC = C * pi / 180
-                cosC = cos(radC)
-                c = sqrt(a * a + b * b - 2 * a * b * cos(radC))
-                cosA = (b * b + c * c - a * a) / (2 * b * c)
-                cosB = (a * a + c * c - b * b) / (2 * a * c)
-                radA = acos(cosA)
-                radB = acos(cosB)
-                halfC = (a + b + c) / 2
-                S = sqrt(halfC * (halfC - a) * (halfC - b) * (halfC - c))
-                Ci = 2 * halfC
+        if int_var.get() == 0:  # When choosing option "SAS"...
+            angle_c = float(angle_c_entry.get())
+            if angle_c < 180:  # The angle of a triangle is never bigger than 180°.
+                edge_a = float(edge_a_entry.get())
+                edge_b = float(edge_b_entry.get())
+                rad_c = angle_c * pi / 180
+                cos_c = cos(rad_c)
+                edge_c = sqrt(edge_a * edge_a + edge_b * edge_b - 2 * edge_a * edge_b * cos(rad_c))
+                cos_a = (edge_b * edge_b + edge_c * edge_c - edge_a * edge_a) / (2 * edge_b * edge_c)
+                cos_b = (edge_a * edge_a + edge_c * edge_c - edge_b * edge_b) / (2 * edge_a * edge_c)
+                rad_a = acos(cos_a)
+                rad_b = acos(cos_b)
+                half_circumference = (edge_a + edge_b + edge_c) / 2
+                area = sqrt(half_circumference * (half_circumference - edge_a) * (half_circumference - edge_b) * (half_circumference - edge_c))
+                circumference = 2 * half_circumference
                 showinfo("Information",
-                         """S = %f.
-C = %f.
-Ir = %f.
-c = %f.
+                         """Area = %f.
+Circumference = %f.
+Inradius = %f.
+Edge c = %f.
 ∠A = %f°.
 ∠B = %f°.
 sinA = %f.
@@ -100,29 +99,29 @@ cosB = %f.
 cosC = %f.
 tanA = %f.
 tanB = %f.
-tanC = %f.""" % (S, Ci, 2 * S / Ci, c, radA / pi * 180,
-                 radB / pi * 180,
-                 sin(radA), sin(radB), sin(radC), cosA, cosB, cosC, tan(radA), tan(radB), tan(radC)))
+tanC = %f.""" % (area, circumference, 2 * area / circumference, edge_c, rad_a / pi * 180,
+                 rad_b / pi * 180,
+                 sin(rad_a), sin(rad_b), sin(rad_c), cos_a, cos_b, cos_c, tan(rad_a), tan(rad_b), tan(rad_c)))
             else:
                 showerror("Error", "This is not a triangle.")
         else:  # When choosing option "SSS"...
-            a = float(ae.get())
-            b = float(be.get())
-            c = float(ce.get())
-            if a + b > c and a + c > b and b + c > a:  # Check whether it is a triangle.
-                halfC = (a + b + c) / 2
-                S = sqrt(halfC * (halfC - a) * (halfC - b) * (halfC - c))
-                Ci = 2 * halfC
-                cosA = (b * b + c * c - a * a) / (2 * b * c)
-                cosB = (a * a + c * c - b * b) / (2 * a * c)
-                cosC = (a * a + b * b - c * c) / (2 * a * b)
-                radA = acos(cosA)
-                radB = acos(cosB)
-                radC = acos(cosC)
+            edge_a = float(edge_a_entry.get())
+            edge_b = float(edge_b_entry.get())
+            edge_c = float(edge_c_entry.get())
+            if edge_a + edge_b > edge_c and edge_a + edge_c > edge_b and edge_b + edge_c > edge_a:  # Check whether it is a triangle.
+                half_circumference = (edge_a + edge_b + edge_c) / 2
+                area = sqrt(half_circumference * (half_circumference - edge_a) * (half_circumference - edge_b) * (half_circumference - edge_c))
+                circumference = 2 * half_circumference
+                cos_a = (edge_b * edge_b + edge_c * edge_c - edge_a * edge_a) / (2 * edge_b * edge_c)
+                cos_b = (edge_a * edge_a + edge_c * edge_c - edge_b * edge_b) / (2 * edge_a * edge_c)
+                cos_c = (edge_a * edge_a + edge_b * edge_b - edge_c * edge_c) / (2 * edge_a * edge_b)
+                rad_a = acos(cos_a)
+                rad_b = acos(cos_b)
+                rad_c = acos(cos_c)
                 showinfo("Information",
-                         """S = %f.
-C = %f.
-Ir = %f.
+                         """Area = %f.
+Circumference = %f.
+Inradius = %f.
 ∠A = %f°.
 ∠B = %f°.
 ∠C = %f°.
@@ -134,14 +133,13 @@ cosB = %f.
 cosC = %f.
 tanA = %f.
 tanB = %f.
-tanC = %f.""" % (S, Ci, 2 * S / Ci, radA / pi * 180, radB / pi * 180,
-                 radC / pi * 180,
-                 sin(radA), sin(radB), sin(radC), cosA, cosB, cosC, tan(radA), tan(radB), tan(radC)))
+tanC = %f.""" % (area, circumference, 2 * area / circumference, rad_a / pi * 180, rad_b / pi * 180,
+                 rad_c / pi * 180,
+                 sin(rad_a), sin(rad_b), sin(rad_c), cos_a, cos_b, cos_c, tan(rad_a), tan(rad_b), tan(rad_c)))
             else:
                 showerror("Error", "This is not a triangle.")
     except ValueError:
         showerror("Error", "Invalid value.")  # When some unexpected things happened...
 
-
-Button(r, text="Process", command=p).grid(row=2, column=0)
-r.mainloop()
+Button(root, text="Process", command=process).grid(row=2, column=0)
+root.mainloop()
